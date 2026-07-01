6d20d9ac517c784503eb8e3b22008fad97b375ac
OctavioFiore <fioreoctavio6@gmail.com>
2026-07-01 15:32:34 -0300

    Agregando el fast api

---
+++ Backend/app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/reservar")
+def verificar_disponibilidad(fecha: str):
+    
+    if fecha == "2026-07-25":
+        return {"status": "ocupado", "mensaje": "Ese día ya está reservado el quincho."}
+    else:
+        return {"status": "disponible", "mensaje": "¡Podés reservar!"}
