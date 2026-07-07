c5923bbdc8f78f8ea4f05fc09ca61ff2cde6d096
OctavioFiore <fioreoctavio6@gmail.com>
2026-07-07 11:14:28 -0300

    feat: init backend structure and add reservation check endpoint

--- Backend/app.py
+++ Backend/app.py
@@ -7,7 +7,7 @@ app = FastAPI()
 @app.get("/reservar")
 def verificar_disponibilidad(fecha: str):
     
-    if fecha == "2026-07-25":
+    if fecha == "2026-07-26":
         return {"status": "ocupado", "mensaje": "Ese día ya está reservado el quincho."}
     else:
         return {"status": "disponible", "mensaje": "¡Podés reservar!"}
